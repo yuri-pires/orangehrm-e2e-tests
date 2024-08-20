112c561f322928e8d8eeb083f29e350c0a8058f1
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-08-20 19:04:46 -0300

    fix: Configs de teste E2E

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
     baseUrl: "https://opensource-demo.orangehrmlive.com",
     watchForFileChanges: false,
